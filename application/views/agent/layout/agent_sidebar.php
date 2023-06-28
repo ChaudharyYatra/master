3f2d66a52d3cc75ce109bec082e55bae96f2e123
vivek <vivek@gmail.com>
2023-06-28 13:37:40 +0530

    changes in agent sidebar fix agent tour enq

--- application/views/agent/layout/agent_sidebar.php
+++ application/views/agent/layout/agent_sidebar.php
@@ -56,7 +56,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="<?php echo base_url(); ?>agent/fixed_Customized_enquiries/index" class="nav-link">
+            <a href="<?php echo base_url(); ?>agent/fixed_customized_enquiries/index" class="nav-link">
               <i class="nav-icon fas fa-book"></i>
               <p>
                Fixed & Customized Enquiries
